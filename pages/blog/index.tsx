2814a1eef76655f9fa3a069247fd719c6fe3ce35
farvellhub <farvellsmurf@gmail.com>
2021-06-14 06:12:21 +0200

    fixed bug on Link blog/index.tsx

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,4 +1,4 @@
-// import Link from "next/link";
+import Link from "next/link";
 // import { formatTitleURLParam } from "../../support";
 // import * as matter from "gray-matter";
 // import fs from "fs";
